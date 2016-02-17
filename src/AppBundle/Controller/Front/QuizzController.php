3a24cfdf6b90cd823b285b8c9a9b312753e524b6
fakebounce <benjamin.romanelli@neuf.fr>
2016-02-17 05:31:39 +0100

    [MOD] Résultats du quizz - Amis + entity

--- src/AppBundle/Controller/Front/QuizzController.php
+++ src/AppBundle/Controller/Front/QuizzController.php
@@ -1,10 +1,12 @@
 <?php
+
 namespace AppBundle\Controller\Front;
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
 
 /**
 * @Route("/quizz")
@@ -34,49 +36,93 @@ class QuizzController extends Controller {
         $em = $this->getDoctrine()->getManager();
         $quizz = $em->getRepository('AppBundle:Quizz')->find($id);
         $questions = $quizz->getQuestions();
-        
         $participationList = [];
         $score_participation = 0;
         $i = 0;
         $rank = 0;
         $maxQuestions = sizeof($questions);
-
-        //A remplacer par la liste d'amis du mec, a voir si faisable
+        $friends_array = array();
+        $friend_score = array();
+        
+        
         // On ne peut afficher que la liste d'amis qui ont l'application, et faut demander une permission supplémentaire
         $quizzParticipation = $em->getRepository('AppBundle:QuizzParticipation')->findBy(["quizz" => $quizz->getId()], ["dataUserFacebook" => "asc"]);
         
         foreach ($quizzParticipation as $participation) {
-                
+            $participant = $participation->getDataUserFacebook()->getProfilName();
+            $participant_pic = $participation->getDataUserFacebook()->getPictureProfilUrl();
+            $participant_id = $participation->getDataUserFacebook()->getId();
             if ($i < $maxQuestions) {
                 $score_participation += (int) $participation->getValid();
                 $i++;
             } else {
                 $participationList[$participant]['score'] = $score_participation;
                 $participationList[$participant]['picture'] = $participant_pic;
+                $participationList[$participant]['id'] = $participant_id;
+                if($_SESSION['user_id'] == $participant_id)
+                {
+                    $friends_array = $em->getRepository('AppBundle:FriendsList')->findBy(["userId" => $participant_id]);
+                }
                 $score_participation = 0;
                 $i = 0;
             }
-            $participant = $participation->getDataUserFacebook()->getProfilName();
-            $participant_pic = $participation->getDataUserFacebook()->getPictureProfilUrl();
         }
         arsort($participationList);
-        /*
         
-        En attente de la session, permettra d'afficher le classement approximatif du joueur
+        // Permet d'afficher le classement approximatif du joueur
         $i = 1;
-        foreach($participationList as $participant => $score)
+        foreach($participationList as $participant)
         {
-            if($_SESSION['name'] == $participant && $score>0)
+            if($_SESSION['user_id'] == $participant['id'] && $participant['score']>0)
             {
                 $rank = $i;
                 break;
             }
             $i++;
         }
-        */
+    
+        $button = '1';
+        if(!empty($_GET['friends']))
+        {
+                
+            $button='0';
+            //On établis les infos des amis dans un tableau
+            foreach ($friends_array as $friend) 
+            {
+                if ($i < $maxQuestions) {
+                    $score_participation += (int) $participation->getValid();
+                    $i++;
+                } else {
+                    $friend_score[$friend['id']]['score'] = $score_participation;
+                    $friend_score[$friend['id']]['name'] = $friend['friendProfilName'];
+                    $friend_score[$friend['id']]['pic'] = $friend['friendPictureProfilName'];
+                    $friend_score[$friend['id']]['rank'] = 0;
+
+                    $score_participation = 0;
+                    $i = 0;
+                }
+            }
+            
+            //On affecte les rangs
+            foreach($friend_score as $friend)
+            {
+                foreach($participationList as $participant)
+                {
+                    $i = 1;
+                    if($friend['pic'] == $participant['picture'] && $participant['score']>0)
+                    {
+                        $friend['rank'] = $i;
+                        break;
+                    }
+                    $i++;
+                }
+            }
+        }
         
         //Décommenter la ligne du dessous pour atterir directement sur la page finale
-       //$id_question = sizeof($questions);
+        
+        
+       $id_question = sizeof($questions);
         if ($id_question < sizeof($questions)) {
             $actualQuestion = $questions[$id_question];
             $rightResponse = $actualQuestion->getResponseValide();
@@ -89,7 +135,8 @@ class QuizzController extends Controller {
                 'score' => $score,
                 'rank' => $rank,
                 'quizz' => $quizz,
-                'participationList' => $participationList
+                'button' => $button,
+                'participationList' => $friend_score
             ]);
         }
 
